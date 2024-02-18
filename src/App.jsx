190a51beae01bc1092bacf9c326f587d5692c8c9
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-02-18 15:19:24 +0000

    add btn style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { Button, Center } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <Center>
+        <Button
+          size="lg"
+          w={"260px"}
+          h={"62px"}
+          pl={6}
+          pr={6}
+          pt={2}
+          pb={2}
+          sx={{
+            background: "var(--blue)",
+            boxShadow:
+              "inset 0px -2px 0px 3px var(--dark-navy), inset 0px 1px 0px 6px var(--blue-transparent)",
+            color: "var(--white)",
+            borderRadius: "40px",
+            _hover: {
+              background: "var(--blue)",
+              boxShadow:
+                "inset 0px -2px 0px 3px var(--blue), inset 0px 1px 0px 6px var(--dark-navy)",
+            },
+            textTransform: "uppercase",
+            fontSize: "30px",
+            letterSpacing: "2px",
+          }}
+        >
+          How to play
+        </Button>
+      </Center>
     </>
   );
 }
