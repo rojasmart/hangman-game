b135c59a1e5ab3de7148247c9a494a6e7b814ee9
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-03-15 20:58:28 +0000

    add array of categories to page

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getCategory } from "../services/game";
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
+  const [categoryData, setCategoryData] = useState(null);
+
+  console.log("categoryData", categoryData);
+
+  useEffect(() => {
+    getCategory(categoryName).then((data) => setCategoryData(data));
+  }, [categoryName]);
+
   return (
     <div>
       <h1>Category Page</h1>
--- src/services/game.js
+++ src/services/game.js
@@ -6,3 +6,12 @@ export async function getGame() {
   const data = await res.json();
   return data?.categories;
 }
+
+export async function getCategory(value) {
+  const res = await fetch("/data.json");
+  if (!res.ok) {
+    throw new Error("Error fetching data");
+  }
+  const data = await res.json();
+  return data?.categories?.[value];
+}
