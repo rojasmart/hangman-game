1b4747d5fc120e9ccbee6c550ace414571ad0cad
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-03-15 21:18:41 +0000

    add random item

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCategory } from "../services/game";
 
+import { Text } from "@chakra-ui/react";
+
 const CategoryPage = () => {
   const { categoryName } = useParams();
   const [categoryData, setCategoryData] = useState(null);
@@ -10,8 +12,10 @@ const CategoryPage = () => {
   useEffect(() => {
     getCategory(categoryName).then((data) => {
       setCategoryData(data);
-      const randomIndex = Math.floor(Math.random() * data.length);
-      setRandomItem(data[randomIndex]);
+      if (Array.isArray(data)) {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        setRandomItem(data[randomIndex]);
+      }
     });
   }, [categoryName]);
 
@@ -22,7 +26,9 @@ const CategoryPage = () => {
     <div>
       <h1>Category Page</h1>
       <p>{categoryName}</p>
+      <Text fontSize={"2xl"}>{randomItem?.name}</Text>
     </div>
   );
 };
+
 export default CategoryPage;
