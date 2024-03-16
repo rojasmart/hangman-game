9a31043578fa8b5afb3ac15d1bc5bc1900b2afda
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-03-16 13:50:35 +0000

    add alphabet to select item

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCategory } from "../services/game";
 
-import { Text } from "@chakra-ui/react";
+import { Text, HStack, Box } from "@chakra-ui/react";
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
@@ -19,6 +19,8 @@ const CategoryPage = () => {
     });
   }, [categoryName]);
 
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
   console.log("categoryData", categoryData);
   console.log("randomItem", randomItem);
 
@@ -27,6 +29,29 @@ const CategoryPage = () => {
       <h1>Category Page</h1>
       <p>{categoryName}</p>
       <Text fontSize={"2xl"}>{randomItem?.name}</Text>
+
+      <HStack flexWrap={"wrap"}>
+        {alphabet.map((letter, index) => (
+          <Box
+            key={index}
+            backgroundColor={"white"}
+            borderRadius={"24px"}
+            w={"109px"}
+            h={"84px"}
+            display={"flex"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+          >
+            <Text
+              fontSize={"3xl"}
+              fontFamily={"Mouse Memoirs"}
+              colorScheme={"var(--dark-navy)"}
+            >
+              {letter}
+            </Text>
+          </Box>
+        ))}
+      </HStack>
     </div>
   );
 };
