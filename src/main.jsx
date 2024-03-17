1f964521bbda13e4e224ac660cb7bd551bbd409a
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-03-17 18:08:45 +0000

    add color bar

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   colors: {
     customColor: {
-      50: "#f2f2f5",
-      100: "#d8d8df",
-      200: "#bfbfc8",
-      300: "#a6a6b2",
-      400: "#8c8c9b",
-      500: "#737384",
-      600: "#59596d",
-      700: "#404056",
-      800: "#26263f",
-      900: "#0d0d29",
+      500: "#261676",
     },
   },
 });
